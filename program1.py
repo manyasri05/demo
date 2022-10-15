89f9f75bce6077e22e801738735ab75a6478c963
manyasri05 <manyasrir05@gmail.com>
2022-10-15 10:03:15 +0530

    updated

--- program1.py
+++ program1.py
@@ -1,7 +1,15 @@
-x=5
-y=10
+x=input('Enter the value of x')
+y=input('Enter the value of y')
+
+
+#print('enter the value of x')
+#print('enter the value of y')
+
+
 temp=x
 x=y
 y=temp
+
+
 print('the value of x after swapping:',x)
 print('the value of y after swapping:',y)
